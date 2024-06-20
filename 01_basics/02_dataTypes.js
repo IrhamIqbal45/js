5309075153eb50e348fdf9a92b36f00cbe354a8c
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-06-20 21:59:32 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hi Cesar_Smith045") // It works in browser.
+let name = "hamza"
+let age = 18
+let loggedIn = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => valued is not assigned yet
+// symbol => uniqueness
+
+//object
+
+console.log(typeof "ali");
