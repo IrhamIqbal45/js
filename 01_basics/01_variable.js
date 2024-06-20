9c25ad6cae4762adc55dc1fd9faa3be55d31a198
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-06-20 21:24:54 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1122
+let accountEmail = "hamza@google.com"
+var accountPassword = "12345678"
+accountCity = "Lahore"
+let accountState
+// accountId = 12 //not allowed
+
+accountEmail = "ali@google.com"
+accountPassword = "87654321"
+accountCity = "Islamabad"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
