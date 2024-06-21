969874f95d4f74d21c0614621210548a8e00a8d2
irhamiqbal45 <irhamiqbal123@gmail.com>
2024-06-21 05:35:40 +0000

    converionOfDataTypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,19 @@
+let score = "33"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNmber);
+
+// console.log(typeof score);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0;
+let isLoginIn = 1
+let booleanIsLoggedIn = Boolean(isLoginIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false; "ali" => true;
+let someNumber = "33"
+let stringNumber = String(someNumber)
+console.log(stringNumber);
